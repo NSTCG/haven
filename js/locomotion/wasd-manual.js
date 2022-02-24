adcd9029447f305cdadbda2258214b4f2acbbd42
NSTCG <nithinsteven32@gmail.com>
2022-02-24 14:00:33 +0530

    Cleaning up wasd + ( joystick enabling for pc )
    Organised wasd-manual
    removed unwanted event listeners
    and added a main controller funtion
    
    changed the joystick to become visible for non - mobile sessions also
    might be helpfull for : quest browser sessions + testings

--- js/locomotion/wasd-manual.js
+++ js/locomotion/wasd-manual.js
@@ -18,18 +18,22 @@ WL.registerComponent(
   },
   {
     init: function () {
+      window.j1x=0;
+      window.j1y=0;
       this.speed = this.normal_speed;
       this.up = false;
       this.right = false;
       this.down = false;
       this.left = false;
 
+      this.up_main = false;
+      this.right_main = false;
+      this.down_main = false;
+      this.left_main = false;
+
       window.addEventListener("keydown", this.press.bind(this));
       window.addEventListener("keyup", this.release.bind(this));
 
-      document.addEventListener("touchstart", this.press.bind(this));
-      document.addEventListener("touchmove", this.press.bind(this));
-      document.addEventListener("touchend", this.reset.bind(this));
     },
 
     start: function () {
@@ -37,12 +41,14 @@ WL.registerComponent(
     },
 
     update: function () {
+
+      this.Main_controller();
       let direction = [0, 0, 0];
 
-      if (this.up) direction[2] -= 1.0;
-      if (this.down) direction[2] += 1.0;
-      if (this.left) direction[0] -= 1.0;
-      if (this.right) direction[0] += 1.0;
+      if (this.up_main) direction[2] -= 1.0;
+      if (this.down_main) direction[2] += 1.0;
+      if (this.left_main) direction[0] -= 1.0;
+      if (this.right_main) direction[0] += 1.0;
 
       vec3.normalize(direction, direction);
       direction[0] *= this.speed;
@@ -50,36 +56,29 @@ WL.registerComponent(
       vec3.transformQuat(direction, direction, this.headObject.transformWorld);
       if (this.restrictY) direction[1] = 0;
       this.object.translate(direction);
+    },
 
-      if (window.j1x > 0.1) {
-        this.object.rotateAxisAngleDegObject([0, 1, 0], -1 * window.j1x);
-      }
-      if (window.j1x < -0.1) {
-        this.object.rotateAxisAngleDegObject([0, 1, 0], -1 * window.j1x);
-      }
-      if (window.j1y < -0.5) {
-        //this.headObject.rotateAxisAngleDeg([1, 0, 0], 1);
-        window.j2y = -1;
-      } else {
-        //window.j2y=0
-      }
-      if (window.j1y > 0.5) {
-        //this.headObject.rotateAxisAngleDeg([1, 0, 0], -1);
-        window.j2y = 1;
-      } else {
-      }
+    Main_controller: function(){
+      if(window.j1x!=null)this.object.rotateAxisAngleDegObject([0, 1, 0], -0.65 * window.j1x);
+      if(window.j1y!=null)this.object.rotateAxisAngleDegObject([1, 0, 0], -0.65 * window.j1y); // vertical rotation ( needs to be fixed)
+
+
+      this.up_main = window.j2y < -0.1  || this.up ? true : false;   //add || window.j1y < -0.1 if  you wanna use left joystick for locomotion too 
+      this.down_main = window.j2y > 0.1 || this.down? true : false;  //add || window.j1y < -0.1 if you wanna use left joystick for locomotion too 
+      this.right_main = window.j2x > 0.1 || this.right ? true : false;  
+      this.left_main = window.j2x < -0.1 ||this.left ? true : false;
     },
 
     press: function (e) {
       if (
         e.keyCode === 38 /* up */ ||
         e.keyCode === 87 /* w */ ||
-        e.keyCode === 90 /* z */
+        e.keyCode === 90 /* z */ 
       ) {
         this.up = true;
-      } else if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */) {
+      } else if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */ ) {
         this.right = true;
-      } else if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */) {
+      } else if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */ ) {
         this.down = true;
       } else if (
         e.keyCode === 37 /* left */ ||
@@ -90,19 +89,7 @@ WL.registerComponent(
       } else if (e.keyCode === 16 /* increment_speed */) {
         this.speed = this.accelerated_speed;
       }
-      if (window.IsMobile) {
-        this.up = window.j2y < -0.1 ? true : false;
-        this.right = window.j2x > 0.1 ? true : false;
-        this.down = window.j2y > 0.1 ? true : false;
-        this.left = window.j2x < -0.1 ? true : false;
-        if (window.j2x == 0 && window.j2y == 0) {
-          this.up = false;
-          this.right = false;
-          this.down = false;
-          this.left = false;
-        }
-        this.speed = this.normal_speed * 0.5;
-      }
+      
     },
 
     release: function (e) {
@@ -127,11 +114,6 @@ WL.registerComponent(
       }
     },
 
-    reset: function (e) {
-      this.up = false;
-      this.right = false;
-      this.down = false;
-      this.left = false;
-    },
+    
   }
 );
